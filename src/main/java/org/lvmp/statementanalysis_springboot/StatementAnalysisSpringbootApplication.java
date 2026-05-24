085c92626b58b761674f141a56a4f789f36b49bb
Lesedi <molemilesedi@gmail.com>
2026-05-24 11:16:05 +0200

    Initial commit

---
+++ src/main/java/org/lvmp/statementanalysis_springboot/StatementAnalysisSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.lvmp.statementanalysis_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatementAnalysisSpringbootApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(StatementAnalysisSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lvmp/statementanalysis_springboot/StatementAnalysisSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lvmp.statementanalysis_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatementAnalysisSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
