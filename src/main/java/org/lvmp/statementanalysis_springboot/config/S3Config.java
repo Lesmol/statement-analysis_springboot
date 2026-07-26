59f9a43fd91bd1ad9df621b574b8ad2d65758e94
Ofentse <lekgetho659@gmail.com>
2026-07-26 16:07:48 +0200

    adding server side encryption

--- src/main/java/org/lvmp/statementanalysis_springboot/config/S3Config.java
+++ src/main/java/org/lvmp/statementanalysis_springboot/config/S3Config.java
@@ -1,17 +1,29 @@
 package org.lvmp.statementanalysis_springboot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.encryption.s3.S3EncryptionClient;
 
 @Configuration
 public class S3Config {
+    @Value("${aws.kms.key-id}")
+    private String kmsKeyId;
 
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.EU_WEST_1)
+                .build();
+    }
+
+    @Bean
+    public S3EncryptionClient s3EncryptionClient() {
+        return s3EncryptionClient().builderV4()
+                .kmsKeyId(kmsKeyId)
+                .enableLegacyUnauthenticatedModes(false)
                 .build();
     }
 }
