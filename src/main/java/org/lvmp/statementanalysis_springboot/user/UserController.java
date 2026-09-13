1b310acc0320ca97f0cb168abc36aa5446d35041
Lesedi Molemi <molemilesedi@gmail.com>
2026-09-13 11:15:57 +0200

    remove unused Slf4j annotation

--- src/main/java/org/lvmp/statementanalysis_springboot/user/UserController.java
+++ src/main/java/org/lvmp/statementanalysis_springboot/user/UserController.java
@@ -1,14 +1,12 @@
 package org.lvmp.statementanalysis_springboot.user;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.lvmp.statementanalysis_springboot.user.internal.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequestMapping("/api/user/v1")
 @RequiredArgsConstructor
